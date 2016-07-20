213a444049425af1e19533da096827a7e8d2d293
damian <dmachtey@gmail.com>
2016-07-20 16:36:17 -0500

    headers

--- include/COIL.h
+++ include/COIL.h
@@ -1,3 +1,38 @@
+// Filename: COIL.h
+// 
+// Description: 
+// Author: Damian Machtey
+// Maintainer: 
+// 
+// Created: Wed Jul 20 16:35:29 2016 (-0500)
+// 
+// Last-Updated: Wed Jul 20 16:35:37 2016 (-0500)
+//           By: Damian Machtey
+//     Update #: 1
+
+// Change Log:
+// 
+// 
+// Copyright (C) 2016 Damian Machtey
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+
+
 
 #ifndef COIL_H
 #define COIL_H
@@ -351,3 +386,6 @@ private:
 }; // end of package namespace
 
 #endif // COIL_H
+
+// 
+// COIL.h ends here
--- src/COIL.cpp
+++ src/COIL.cpp
@@ -1,3 +1,37 @@
+// Filename: COIL.cpp
+// 
+// Description: 
+// Author: Damian Machtey
+// Maintainer: 
+// 
+// Created: Wed Jul 20 16:34:45 2016 (-0500)
+// 
+// Last-Updated: Wed Jul 20 16:35:00 2016 (-0500)
+//           By: Damian Machtey
+//     Update #: 1
+
+// Change Log:
+// 
+// 
+// Copyright (C) 2016 Damian Machtey
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+// 
+// 
+
+// Code:
+
 #include "COIL.h"
 
 // Constructors/Destructors
@@ -33,3 +67,6 @@ void COIL::initAttributes () {
   total_power_all_clases = 0.0;
 }
 
+
+// 
+// COIL.cpp ends here
